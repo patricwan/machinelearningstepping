07ead1cdb674e48979dae4b372a32f2a2c35cc85
patric wan <shuyin_80@126.com>
2019-03-02 21:38:14 -0500

    add more regression examples

---
+++ CommonUtils.py
@@ -0,0 +1,39 @@
+import pandas as pd 
+import numpy as np
+
+def trimList(originalList):
+    newTripList = []
+    for item in originalList[:]:
+        newTripList.append(item.strip())    
+    return newTripList
+
+def mapColumnToNum(dataFrame,columnName):
+    dfColsArray = pd.unique(dataFrame[columnName])
+    dfColsList = dfColsArray.tolist()
+    newTripList = trimList(dfColsList)
+    dfCols = dataFrame[columnName]
+    dfCols = dfCols.map(lambda eachStr: newTripList.index(eachStr.strip()))
+    return trainWorkclass
+
+import zipfile
+#unzip a zip file to current folder *.zip
+def unzipFile(zipFile):
+    zfile = zipfile.ZipFile(zipFile,'r')
+    for filename in zfile.namelist():
+      data = zfile.read(filename)
+      file = open(filename, 'w+b')
+      file.write(data)
+      file.close()    
+    return 0
+
+import os
+import glob
+
+#Delete files with path/name pattern, eg:  deleteFileByPattern("./RentListingInquries*")
+def deleteFileByPattern(fileNamePattern):
+    for fileName in glob.glob(fileNamePattern):
+       print(fileName)
+       os.remove(fileName)
+    return 1
+
+
